1688c0dc1610231ebd995a97bfa0b890ec82a3ab
trandat1537 <101917578+trandat1537@users.noreply.github.com>
2022-04-05 23:11:14 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     titleWeb: "Tiêu đề web",
-    introTitle: 'Nguyễn Bích Ngọc',
+    introTitle: 'Nguyễn Thanh Tâm',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
@@ -16,5 +16,5 @@ const CONFIG = {
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
     messDesc: 'Hôm nào rảnh có thể cho mình mời bạn đi chơi nha.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    messLink: 'https://www.facebook.com/messages/t/100018655006107' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
